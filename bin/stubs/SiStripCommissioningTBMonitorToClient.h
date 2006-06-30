139e318aaa167adb905cef08a475331c9627b375
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2006-06-30 13:51:48 +0000

    converts a TBMonitor file to a client file

---
+++ bin/stubs/SiStripCommissioningTBMonitorToClient.h
@@ -0,0 +1,73 @@
+#ifndef DQM_SiStripCommissioningSummary_SiStripCommissioningTBMonitorToClient_h
+#define DQM_SiStripCommissioningSummary_SiStripCommissioningTBMonitorToClient_h
+
+#include <map>
+#include <string>
+#include <vector>
+
+//common
+#include "DQM/SiStripCommon/interface/SiStripHistoNamingScheme.h"
+
+//DQM/SiStripCommissioningSummary
+#include "DQM/SiStripCommissioningSummary/bin/stubs/SiStripCommissioningFile.h"
+
+//root
+#include "TProfile.h"
+#include "TDirectory.h"
+
+/** 
+    @file : DQM/SiStripCommissioningSummary/bin/stubs/SiStripCommissioningTBMonitorToClient.h
+    @class : SiStripCommissioningTBMonitorToClient
+    @author : M.Wingham
+
+    @brief : Reads in TBMonitor file(s), renames the TProfiles of interest (in accordance with the scheme in DQM/SiStripCommon/interface/SiStripHistoNamingScheme.h) and writes them to a "client" file. 
+*/
+
+class SiStripCommissioningTBMonitorToClient {
+
+ public:
+
+  /** Constructor */
+  explicit SiStripCommissioningTBMonitorToClient(std::string& client_path, std::string& tb_path, sistrip::Task = sistrip::PEDESTALS);
+
+  /** Destructor */
+  ~SiStripCommissioningTBMonitorToClient();
+
+  /** Reads TBMonitor file(s), extracts TProfiles that match the commissioning task of interest, reformats them into "client" histograms (naming scheme as defined in DQM/SiStripCommon/interface/SiStripHistoNamingScheme.h) and writes them to file.*/
+  bool convert();  
+
+  /** Sets run number based on file name(s) */
+  void setRunInfo();
+
+ private:
+  
+  /** Takes task name as the argument and updates the "task id" : a common sub-string of the names of histograms for storage.*/
+  static std::string taskId(sistrip::Task);
+
+  /** Unpacks TBMonitor histogram name. */
+  SiStripHistoNamingScheme::HistoTitle histoTitle(const std::string&);
+  
+ /** Splits Pedestals Profile on the module-level into 2 or 3 Profiles on the LLD-channel-level.*/
+  void lldPedestals(TProfile& module, std::vector<TProfile>& llds); 
+  
+  /** TBMonitor file, Client file. */
+  std::string tb_path_, client_path_;
+
+  /** Commissioning task. */
+  sistrip::Task task_;
+
+  /** Commissioning File View */
+  sistrip::View view_;
+
+  /** TBMonitor file.  Client file. */
+  SiStripCommissioningFile* tb_, *client_;
+
+  /** Task id found in TBMonitor histogram title */
+  std::string taskId_;
+
+  /** Run number */
+  unsigned int run_;
+ 
+};
+
+#endif // DQM_SiStripCommissioningSummary_SiStripCommissioningTBMonitorToClient_h
