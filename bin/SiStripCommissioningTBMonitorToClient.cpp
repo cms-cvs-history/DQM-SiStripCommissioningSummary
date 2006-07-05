be6d9fafec7fd02cc3927c42c5a46f05c3aca6c5
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2006-07-05 19:15:51 +0000

    small changes

--- bin/SiStripCommissioningTBMonitorToClient.cpp
+++ bin/SiStripCommissioningTBMonitorToClient.cpp
@@ -13,27 +13,22 @@ using namespace std;
 
 /** 
     @author : M.Wingham
-
     @brief : Reads in Commissioning 
     TBMonitor-histogram file(s) and writes client TProfiles to file. 
 */
 
-//-----------------------------------------------------------------------------
-
-void messageService(void) {
-
-  edm::AssertHandler ah;
-  boost::shared_ptr<edm::Presence> theMessageServicePresence = boost::shared_ptr<edm::Presence>(edm::PresenceFactory::get()->makePresence("MessageServicePresence").release());}
-
-//-----------------------------------------------------------------------------
-
 int main(void) {
 
-  messageService();
-
-  string tb_path = "/afs/cern.ch/user/p/pwing/public/TBHistos/TBMonitor0030349_000.root";
+  //message logger info to standard out.
+  edm::AssertHandler ah;
+  boost::shared_ptr<edm::Presence> theMessageServicePresence = boost::shared_ptr<edm::Presence>(edm::PresenceFactory::get()->makePresence("MessageServicePresence").release());
+  
+  //configure
+  string tb_path = "/afs/cern.ch/user/p/pwing/public/TBHistos/TBMonitor0030345_000.root";
   string client_path = "Client.root";
-  sistrip::Task task = sistrip::PEDESTALS;
+  sistrip::Task task = sistrip::OPTO_SCAN;
+
+  //run
   SiStripCommissioningTBMonitorToClient tbToClient(tb_path,client_path,task);
   if(!tbToClient.convert()) {cout << "[main]: Error with TBMonitor conversions. TBMonitor file is either not-present or contains no TH1F histograms. " << endl;}
   return 0;
--- bin/SiStripOfflineCommissioningClient.cpp
+++ bin/SiStripOfflineCommissioningClient.cpp
@@ -16,24 +16,18 @@ using namespace std;
 and sumarises. Summary histograms are written to a separate file. 
 */
 
-//-----------------------------------------------------------------------------
-
-void messageService(void) {
-
-  edm::AssertHandler ah;
-  boost::shared_ptr<edm::Presence> theMessageServicePresence = boost::shared_ptr<edm::Presence>(edm::PresenceFactory::get()->makePresence("MessageServicePresence").release());}
-
-//-----------------------------------------------------------------------------
-
 int main(void) {
 
-  messageService();
-
-  //define arguments
+  //message logger info to standard out.
+   edm::AssertHandler ah;
+   boost::shared_ptr<edm::Presence> theMessageServicePresence = boost::shared_ptr<edm::Presence>(edm::PresenceFactory::get()->makePresence("MessageServicePresence").release());
+  
+  //configure
   string client_path = "Client_0030349.root";
   string summary_path = "Summary";
-  string summaryLevel = "ControlView/";
+  string summaryLevel = "ControlView/FecCrate0/FecSlot2/FecRing7/CcuAddr123/";
 
+  //run
   SiStripOfflineCommissioningClient offlineClient(client_path, summary_path, summaryLevel);
   offlineClient.analysis();
   return 0;
