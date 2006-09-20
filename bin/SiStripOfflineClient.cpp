79d794d10806cb5713886ca9f0d0f7ff06ac609d
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2006-09-20 09:32:39 +0000

    added read in from config file

--- bin/SiStripOfflineClient.cpp
+++ bin/SiStripOfflineClient.cpp
@@ -26,18 +26,157 @@ using namespace std;
 int main( int argc, char* argv[] ) {
 
   // Default values for arguments
-  string file = "client.root";
+
+  std::string cfg_file = "client_cfg.dat";
+  std::string file = "client.root";
+  std::string level  = "ControlView";
   sistrip::SummaryHisto histo = sistrip::UNKNOWN_SUMMARY_HISTO;
   sistrip::SummaryType type = sistrip::UNKNOWN_SUMMARY_TYPE;
   sistrip::Granularity gran = sistrip::UNKNOWN_GRAN;
-  string level  = "FecCrate0";
   
-  // Read in args to main
-  if ( argc > 1 ) { file  = argv[1]; }
-  if ( argc > 2 ) { histo = static_cast<sistrip::SummaryHisto>( atoi( argv[2] ) ); }
-  if ( argc > 3 ) { type  = static_cast<sistrip::SummaryType>( atoi( argv[3] ) ); }
-  if ( argc > 4 ) { gran  = static_cast<sistrip::Granularity>( atoi( argv[4] ) ); }
-  if ( argc > 5 ) { level = argv[5]; }
+  // Read in arguments to main
+
+  if ( argc > 1 ) { 
+    cfg_file  = argv[1]; 
+    cout << "Reading config. file: " << cfg_file << endl;
+    ifstream in;
+    in.open( cfg_file.c_str() );
+    if( !in ) {
+      stringstream ss;
+      ss  << "[" << __PRETTY_FUNCTION__ << "]"
+	  << " File could not be opened at '" 
+	  << cfg_file << "'";
+      cerr << ss.str() << endl;
+    } else {
+      while ( !in.eof() ) {
+	string data;
+	getline(in,data); 
+
+	//interpret data here....
+
+	string::size_type pos = data.find("ClientFile=",0);
+	if (pos != string::npos) {
+	  file = data.substr(pos+11);}
+
+	pos = data.find("SummaryLevel=",0);
+	if (pos != string::npos) {
+	  level = data.substr(pos+13);}
+	
+	pos = data.find("SummaryHisto=",0);
+	if (pos != string::npos) {
+	  string summaryhisto = data.substr(pos+13);
+	  if (summaryhisto == sistrip::undefinedSummaryHisto_) {
+	    histo = sistrip::UNDEFINED_SUMMARY_HISTO;}
+	  else if (summaryhisto == sistrip::undefinedSummaryHisto_) {
+	    histo = sistrip::UNDEFINED_SUMMARY_HISTO;}
+	  else if (summaryhisto == sistrip::apvTimingTime_) {
+	    histo = sistrip::APV_TIMING_TIME;}
+	  else if (summaryhisto == sistrip::apvTimingMax_) {
+	    histo = sistrip::APV_TIMING_MAX_TIME;}
+	  else if (summaryhisto == sistrip::apvTimingDelay_) {
+	    histo = sistrip::APV_TIMING_DELAY;}
+	  else if (summaryhisto == sistrip::apvTimingError_) {
+	    histo = sistrip::APV_TIMING_ERROR;}
+	  else if (summaryhisto == sistrip::apvTimingBase_) {
+	    histo = sistrip::APV_TIMING_BASE;}
+	  else if (summaryhisto == sistrip::apvTimingPeak_) {
+	    histo = sistrip::APV_TIMING_PEAK;}
+	  else if (summaryhisto == sistrip::apvTimingHeight_) {
+	    histo = sistrip::APV_TIMING_HEIGHT;}
+	  else if (summaryhisto == sistrip::fedTimingTime_) {
+	    histo = sistrip::FED_TIMING_TIME;}
+	  else if (summaryhisto == sistrip::fedTimingMax_) {
+	    histo = sistrip::FED_TIMING_MAX_TIME;}
+	  else if (summaryhisto == sistrip::fedTimingDelay_) {
+	    histo = sistrip::FED_TIMING_DELAY;}
+	  else if (summaryhisto == sistrip::fedTimingError_) {
+	    histo = sistrip::FED_TIMING_ERROR;}
+	  else if (summaryhisto == sistrip::fedTimingBase_) {
+	    histo = sistrip::FED_TIMING_BASE;}
+	  else if (summaryhisto == sistrip::fedTimingPeak_) {
+	    histo = sistrip::FED_TIMING_PEAK;}
+	  else if (summaryhisto == sistrip::fedTimingHeight_) {
+	    histo = sistrip::FED_TIMING_HEIGHT;}
+	  else if (summaryhisto == sistrip::optoScanLldBias_) {
+	    histo = sistrip::OPTO_SCAN_LLD_BIAS_SETTING;}
+	  else if (summaryhisto == sistrip::optoScanLldGain_) {
+	    histo = sistrip::OPTO_SCAN_LLD_GAIN_SETTING;}
+	  else if (summaryhisto == sistrip::optoScanMeasGain_) {
+	    histo = sistrip::OPTO_SCAN_MEASURED_GAIN;}
+	  else if (summaryhisto == sistrip::optoScanZeroLight_) {
+	    histo = sistrip::OPTO_SCAN_ZERO_LIGHT_LEVEL;}
+	  else if (summaryhisto == sistrip::optoScanLinkNoise_) {
+	    histo = sistrip::OPTO_SCAN_LINK_NOISE;}
+	  else if (summaryhisto == sistrip::optoScanBaseLiftOff_) {
+	    histo = sistrip::OPTO_SCAN_BASELINE_LIFT_OFF;}
+	  else if (summaryhisto == sistrip::optoScanLaserThresh_) {
+	    histo = sistrip::OPTO_SCAN_LASER_THRESHOLD;}
+	  else if (summaryhisto == sistrip::optoScanTickHeight_) {
+	    histo = sistrip::OPTO_SCAN_TICK_HEIGHT;}
+	  else if (summaryhisto == sistrip::vpspScanBothApvs_) {
+	    histo = sistrip::VPSP_SCAN_BOTH_APVS;}
+	  else if (summaryhisto == sistrip::vpspScanApv0_) {
+	    histo = sistrip::VPSP_SCAN_APV0;}
+	  else if (summaryhisto == sistrip::vpspScanApv1_) {
+	    histo = sistrip::VPSP_SCAN_APV1;}
+	  else if (summaryhisto == sistrip::pedestalsMean_) {
+	    histo = sistrip::PEDESTALS_MEAN;}
+	  else if (summaryhisto == sistrip::pedestalsSpread_) {
+	    histo = sistrip::PEDESTALS_SPREAD;}
+	  else if (summaryhisto == sistrip::pedestalsMax_) {
+	    histo = sistrip::PEDESTALS_MAX;}
+	  else if (summaryhisto == sistrip::pedestalsMin_) {
+	    histo = sistrip::PEDESTALS_MIN;}
+	  else if (summaryhisto == sistrip::noiseMean_) {
+	    histo = sistrip::NOISE_MEAN;}
+	  else if (summaryhisto == sistrip::noiseSpread_) {
+	    histo = sistrip::NOISE_SPREAD;}
+	  else if (summaryhisto == sistrip::noiseMax_) {
+	    histo = sistrip::NOISE_MAX;}
+	  else if (summaryhisto == sistrip::noiseMin_) {
+	    histo = sistrip::NOISE_MIN;}
+	  else if (summaryhisto == sistrip::numOfDead_) {
+	    histo = sistrip::NUM_OF_DEAD;}
+	  else if (summaryhisto == sistrip::numOfNoisy_) {
+	    histo = sistrip::NUM_OF_NOISY;}}
+
+	pos = data.find("SummaryType=",0);
+	if (pos != string::npos) {
+	  string summarytype = data.substr(pos+12);
+	  if (summarytype == sistrip::undefinedSummaryType_) {
+	    type = sistrip::UNDEFINED_SUMMARY_TYPE;}
+	  else if (summarytype == sistrip::summaryDistr_) {
+	    type = sistrip::SUMMARY_DISTR;}
+	  else if (summarytype == sistrip::summary1D_) {
+	    type = sistrip::SUMMARY_1D;}
+	  else if (summarytype == sistrip::summary2D_) {
+	    type = sistrip::SUMMARY_2D;}
+	  else if (summarytype == sistrip::summaryProf_) {
+	    type = sistrip::SUMMARY_PROF;}}
+
+	pos = data.find("SummaryGran=",0);
+	if (pos != string::npos) {
+	  string granularity = data.substr(pos+12);
+	  if (granularity == sistrip::tracker_) {
+	    gran = sistrip::TRACKER;}
+	  else if (granularity == sistrip::partition_) {
+	    gran = sistrip::PARTITION;}
+	  else if (granularity == sistrip::module_) {
+	    gran = sistrip::CCU_CHAN;}
+	  else if (granularity == sistrip::lldChan_) {
+	    gran = sistrip::LLD_CHAN;}
+	  else if (granularity == sistrip::apv_) {
+	    gran = sistrip::APV;}}
+      }
+      in.close();
+    }
+  }
+
+  if ( argc > 2 ) { file  = argv[2]; }
+  if ( argc > 3 ) { histo = static_cast<sistrip::SummaryHisto>( atoi( argv[3] ) ); }
+  if ( argc > 4 ) { type  = static_cast<sistrip::SummaryType>( atoi( argv[4] ) ); }
+  if ( argc > 5 ) { gran  = static_cast<sistrip::Granularity>( atoi( argv[5] ) ); }
+  if ( argc > 6 ) { level = argv[6]; }
   
   cout << "SiStripOfflineClient:" << endl
 	    << " file:        " << file << endl
@@ -86,7 +225,7 @@ int main( int argc, char* argv[] ) {
       }
       in.close();
       cout << "[" << __PRETTY_FUNCTION__ << "]"
-	   << " Configuration read from '" 
+	   << " Message Logger configuration read from '" 
 	   << filename.str() << "' file is: " << endl
 	   << config.str() << endl;
     }
